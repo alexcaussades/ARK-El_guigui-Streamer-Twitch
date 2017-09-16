f30bc2979759d68b43113a62182f55beff6f6e49
alexcaussades <alexcaussades@gmail.com>
2017-09-16 21:50:10 +0000

    Add new file

---
+++ test.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<head>
+<!-- code créé par Alexcaussades pour le compte de El_GuiGui toute reproduction et formellement interdite 
+contact pro: alexcaussades@gmail.com twitter: @alexcaussades -->
+
+<!-- Google / Copyright / Langue / Robots / Title (**Pas d'indexation**) -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="googlebot" content="noindex, nofollow">
+  <meta name="robots" content="noindex, nofollow">
+  <meta name="author" content="alexcaussades" />
+  <meta name="language" content="FR" />
+  <meta name="copyright" content="alexcaussades™ © 2017" />
+<!-- Code CSS / Bootstrap / fontawesome -->
+
+<!-- CSS -->
+  <link rel="stylesheet" type="text/css" href="assets/css/webtv.css">
+
+<!-- fontawesome -->
+  <script src="https://use.fontawesome.com/4ff14410c4.js"></script>
+
+<!-- Bootstrap -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+
+<!-- information menu (CSS: social(CSS: social(haut du site à droite) responsive ok)-->
+
+
+<!-- Block de gauche / menus / logo / separateur / social -->
+<div class="menu-gauche">
+<br />
+
+<!-- Class="logo" -->
+<div class="logo"><img src="assets/img/la7compagnie.png" width="80%" height="80%"></div>
+
+<!-- reseau social (CSS: fa / social) (ouverture sous une nouvelle fenetre (target="_blank") )  -->
+<div class="social"><a href="https://www.twitch.tv/la_7_compagnie" target="_blank"><i class="fa fa-twitch" aria-hidden="true"></i></a>&nbsp;
+    <a href="https://www.facebook.com/7compagnie/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>&nbsp;
+    <a href="https://twitter.com/7_compagnie" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>&nbsp;
+    <a href="https://www.youtube.com/channel/UCeiG2LpFtneOspdupw7C7Zw" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a></div>
+
+<br />
+
+<!-- séparateur du menu -->
+<div id="bar"><img src="http://ark.elguigui.fr/webtv/assets/img/green.png"></div>
+
+
+<!-- Menu avec modification au passage de la souri -->
+<div class="menu">
+<ul><i class="fa fa-refresh fa-spin fa-1x fa-fw" aria-hidden="true"></i> Accueil</ul>
+<ul><i class="fa fa-th-list" aria-hidden="true"></i> Programme</ul>
+<ul><i class="fa fa-pencil fa-fw" aria-hidden="true"></i> Nous rejoindre</ul>
+<ul><i class="fa fa-book fa-fw" aria-hidden="true"></i> A propos</ul>
+</div>
+</div>
+
+
+<!-- décente du player de twitch sur la page principal -->
+<br /><br /><br /><br /><br /><br /><br /><br />
+
+
+<!-- player & chat de twitch (CSS: twitch / chat / centrage)  -->
+<div class="twitch"><iframe src="https://player.twitch.tv/?channel=la_7_compagnie" frameborder="0" allowfullscreen="true" scrolling="no" height="100%" width="100%"></iframe><iframe class="chat" src="https://www.twitch.tv/la_7_compagnie/chat?popout=" frameborder="0" scrolling="no" height="100%" width="100%"></iframe></div>
+
