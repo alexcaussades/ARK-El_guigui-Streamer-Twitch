4b99bbf84e8b7994588df8109ad41d748bf1117f
alexcaussades <alexcaussades@gmail.com>
2017-09-17 17:42:01 +0200

    mod auto (reverted from commit e49f58c105c5825bb1b72852bacb55950422e11b)

---
+++ Index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<!-- code créé par Alexcaussades pour le compte de El_GuiGui toute reproduction et formellement interdite 
+contact pro: alexcaussades@gmail.com twitter: @alexcaussades -->
+
+<head>
+<!-- Google / Copyright / Langue / Robots / Title -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="googlebot" content="index, follow">
+	<meta name="robots" content="index, follow">
+	<meta name="author" content="alexcaussades" />
+	<meta name="language" content="FR" />
+	<meta name="copyright" content="alexcaussades™ © 2017" />
+	<meta name="google-site-verification" content="n8G_ihdQNPpkWHAnBuLUCoggjEKAwLAt4If3ClehYNc" />
+  <title>ARK OF THRONES | SERVEUR RP SUR LE JEU ARK</title>
+  <!-- media social / Facebook (og) / Twitter (Twitter card) -->
+<link rel="canonical" href="http://ark.elguigui.fr/" />
+<meta property="og:title" content="ARK OF THRONES | SERVEUR RP SUR LE JEU ARK" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://ark.elguigui.fr/" />
+<meta property="og:image" content="http://ark.elguigui.fr/assets/img/logoark.png" />
+<meta property="og:site_name" content="ARK OF THRONES | SERVEUR RP SUR LE JEU ARK" />
+<meta property="fb:admins" content="434911526718854" />
+<meta property="og:description" content="ARK OF THRONES serveur RP de El_GuiGui Streamer de jeux vidéo sur Twitch" />
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="@guigui7794" />
+<meta name="twitter:title" content="ARK OF THRONES | SERVEUR RP SUR LE JEU ARK" />
+<meta name="twitter:description" content="ARK OF THRONES serveur RP de El_GuiGui Streamer de jeux vidéo sur Twitch"/>
+<meta name="twitter:image" content="http://ark.elguigui.fr/assets/img/logoark.png" />
+<!-- Code CSS / Bootstrap / google fonts / descpription / icon -->
+	<meta name="description" content="ARK OF THRONES serveur RP de El_GuiGui Streamer de jeux vidéo sur Twitch" />
+      <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="assets/css/attente.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+      <script src="https://use.fontawesome.com/4ff14410c4.js"></script>
+      <link rel="icon" type="image/png" href="http://ark.elguigui.fr/assets/img/logoark.png" />
+<!-- Blocage de clique droit -->
+<SCRIPT LANGUAGE="JavaScript"> 
+<!-- Disable 
+function disableselect(e){ 
+return false 
+} 
+
+function reEnable(){ 
+return true 
+} 
+
+//if IE4+ 
+document.onselectstart=new Function ("return false") 
+document.oncontextmenu=new Function ("return false") 
+//if NS6 
+if (window.sidebar){ 
+document.onmousedown=disableselect 
+document.onclick=reEnable 
+} 
+//--> 
+</script> 
+<!-- Fin de Blocage de clique droit -->
+</head>
+
+<body>
+
+    <!-- Video en background / (CSS: vidéoacid) -->
+    <div class="video-container">
+            <video autoplay loop class="fillWidth" id="videoacid">
+                <source src="./assets/video/video-n02.mp4" type="video/mp4">
+                Your browser does not support the video tag. I suggest you upgrade your browser.
+            </video>
+        </div>
+    
+    <!-- réglage du volume de la vidèo (Volume par défaut 0.1) -->
+    <script type="text/javascript"> var monElementAudio = document.getElementById('videoacid');
+    monElementAudio.volume = 0.1;
+    </script>
+
+        <!-- Logo ark of thrones mobile (CSS: imageres) -->
+        <img class="imageres" src="http://ark.elguigui.fr/assets/img/logoark.png" alt="Logo ARK OF THRONES">
+
+<!-- reseau social (CSS: fa / social) (ouverture sous une nouvelle fenetre (target="_blank") )  -->
+          <br><br><br>
+          <div class="row social">
+          <a href="https://www.twitch.tv/el_guigui" target="_blank"><i class="fa fa-twitch" aria-hidden="true"></i></a>&nbsp;
+          <a href="https://www.facebook.com/Guigui7710/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>&nbsp;
+          <a href="https://twitter.com/guigui7794" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>&nbsp;
+          <a href="https://www.youtube.com/channel/UCOFnr2ac2ANj65Y7OQ-mSlg" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+          </div>
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+<!-- Début du script de presentation Botton (CSS: btn2) / twitter / discord / compte a rebour (CSS: car) -->
+              <div class="container">
+              <div class="row">
+                <div class="col-xs-6 col-md-4"></div>
+                <div class="col-md-6 col-md-offset-3">
+                <a href="http://elguigui.fr/whitelist-ark/" class="btn2" role="button">Commencer l'aventure !</a>
+                  <br />
+                      <center><a href="https://twitter.com/intent/tweet?button_hashtag=arkofthrones&text=Venez%20me%20rejoindre%20sur%20le%20serveur%20d'ARK%20OF%20THRONES" class="twitter-hashtag-button" data-size="large" data-related="guigui7794" data-url="http://ark.elguigui.fr/">Tweet #arkofthrones</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+                          </script>
+                          <a href="https://discord.gg/ZfHF3y7"><img class="discord" src="./assets/img/discord.png" alt="Rejoint-nous sur discord"></a></center></center>
+                            <br />
+                                  <span class="car">
+               
+                                    <script language="JavaScript">
+                                     
+                                    TargetDate = "09/18/2017 9:00 PM";
+                                     
+                                    BackColor = false;
+                                     
+                                    ForeColor = false;
+                                     
+                                    CountActive = true;
+                                     
+                                    CountStepper = -1;
+                                     
+                                    LeadingZero = true;
+                                     
+                                    DisplayFormat = "%%D%% Jours %%H%% Heures %%M%% Minutes %%S%% Secondes";
+                                     
+                                    FinishMessage = "Le serveur et ouvert !";
+                                     
+                                    </script>
+               
+                                        <script language="JavaScript" src="http://scripts.hashemian.com/js/countdown.js">
+                                         
+                                        </script></span>
+               
+                                      &nbsp;
+                                      </div>
+<!-- Pas de mise en place de prevue sur la col-xs-6 col-md-4 -->
+  <div class="col-xs-6 col-md-4"></div>
+</div></div>
+<!-- social média version mobile (CSS: FA1) -->
+<div class="row fa1 famobile">
+<a href="https://www.twitch.tv/el_guigui"><i class="fa1 fa-twitch" aria-hidden="true"></i></a>&nbsp;
+<a href="https://www.facebook.com/Guigui7710/"><i class="fa1 fa-facebook" aria-hidden="true"></i></a>&nbsp;
+<a href="https://twitter.com/guigui7794"><i class="fa1 fa-twitter" aria-hidden="true"></i></a>&nbsp;
+<a href="https://www.youtube.com/channel/UCOFnr2ac2ANj65Y7OQ-mSlg"><i class="fa1 fa-youtube" aria-hidden="true"></i></a> 
+</div>
+<br /><br /><br /><br />
+<!-- copyright el_guigui 2017 (A modifier en 2018) -->
+<div class="copyright">© 2017 El_GuiGui Toute reproduction interdite sans l'autorisation de l'auteur.</div>
+</body>
+</html>
